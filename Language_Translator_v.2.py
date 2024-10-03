6efd5237454f81fcd128f2777395f690c4224776
unknown <mariateresamaedj19@gmail.com>
2024-10-04 00:59:23 +0800

    Languages Added

--- Language_Translator_v.2.py
+++ Language_Translator_v.2.py
@@ -17,3 +17,176 @@ frame1.place(x=0, y=0)
 
 Label(root, text="☆*: .｡.Language Translator ᓚᘏᗢ.｡.:*☆", font= ("Script 23 bold"), fg="black", bg='white').pack(pady=10)
 
+def translate():
+    lang_1 = text_entry1.get("1.0", "end-1c")
+    cl = choose_language.get()
+
+    if lang_1 == '':
+        messagebox.showerror('Language Translator', 'Enter the Text to Translate!')
+    else:
+        text_entry2.delete(1.0, 'end')
+        translator = Translator()
+        output = translator.translate(lang_1, dest=cl)
+        text_entry2.insert('end', output.text)
+
+def clear():
+    text_entry1.delete(1.0, 'end')
+    text_entry2.delete(1.0, 'end')
+
+a = tk.StringVar()
+
+auto_select = ttk.Combobox(frame1, width=27, textvariable= a, state='randomly', font=('Arial', 10, 'bold'))
+
+auto_select['values'] = (
+                         'Auto Select',
+                        )
+
+auto_select.place(x=30, y=60)
+auto_select.current(0)
+
+l = tk.StringVar()
+choose_language = ttk.Combobox(frame1, width=27, textvariable=l, state='readonly', font=('Arial', 10, 'bold'))
+choose_language['values'] = (
+                        'Afrikaans',
+                        'Albanian',
+                        'Amharic',
+                        'Arabic',
+                        'Armenian',
+                        'Assamese',
+                        'Aymara',
+                        'Azerbaijani',
+                        'Bambara',
+                        'Basque',
+                        'Belarusian',
+                        'Bengali',
+                        'Bhojpuri',
+                        'Bosnian',
+                        'Bulgarian',
+                        'Catalan',
+                        'Cebuano',
+                        'Chinese',
+                        'Corsican',
+                        'Croatian',
+                        'Czech',
+                        'Danish',
+                        'Dhivehi',
+                        'Dogri',
+                        'Dutch',
+                        'English',
+                        'Esperanto',
+                        'Estonian',
+                        'Ewe',
+                        'Filipino',
+                        'Finnish',
+                        'French',
+                        'Frisian',
+                        'Galician',
+                        'Georgian',
+                        'German',
+                        'Greek',
+                        'Guarani',
+                        'Gujarati',
+                        'Haitian Creole',
+                        'Hausa',
+                        'Hawaiian',
+                        'Hebrew',
+                        'Hindi',
+                        'Hmong',
+                        'Hungarian',
+                        'Icelandic',
+                        'Igbo',
+                        'Ilocano',
+                        'Indonesian',
+                        'Irish',
+                        'Italian',
+                        'Japanese',
+                        'Javanese',
+                        'Kannada',
+                        'Kazakh',
+                        'Khmer',
+                        'Kinyarwanda',
+                        'Konkani',
+                        'Korean',
+                        'Krio',
+                        'Kurdish',
+                        'Kurdish',
+                        'Kyrgyz',
+                        'Lao',
+                        'Latin',
+                        'Latvian',
+                        'Lingala',
+                        'Lithuanian',
+                        'Luganda',
+                        'Luxembourgish',
+                        'Macedonian',
+                        'Maithili',
+                        'Malagasy',
+                        'Malay',
+                        'Malayalam',
+                        'Maltese',
+                        'Maori',
+                        'Marathi',
+                        'Meiteilon',
+                        'Mizo',
+                        'Mongolian',
+                        'Myanmar',
+                        'Nepali',
+                        'Norwegian',
+                        'Nyanja',
+                        'Odia',
+                        'Oromo',
+                        'Pashto',
+                        'Persian',
+                        'Polish',
+                        'Portuguese',
+                        'Punjabi',
+                        'Quechua',
+                        'Romanian',
+                        'Russian',
+                        'Samoan',
+                        'Sanskrit',
+                        'Scots Gaelic',
+                        'Sepedi',
+                        'Serbian',
+                        'Sesotho',
+                        'Shona',
+                        'Sindhi',
+                        'Sinhala',
+                        'Slovak',
+                        'Slovenian',
+                        'Somali',
+                        'Spanish',
+                        'Sundanese',
+                        'Swahili',
+                        'Swedish',
+                        'Tajik',
+                        'Tamil',
+                        'Tatar',
+                        'Telugu',
+                        'Thai',
+                        'Tigrinya',
+                        'Tsonga',
+                        'Turkish',
+                        'Turkmen',
+                        'Twi',
+                        'Ukrainian',
+                        'Urdu',
+                        'Uyghur',
+                        'Uzbek',
+                        'Vietnamese',
+                        'Welsh',
+                        'Xhosa',
+                        'Yiddish',
+                        'Yoruba',
+                        'Zulu',
+                            )
+choose_language.place(x=335, y=60)
+choose_language.current(0)
+
+text_entry1 = Text(frame1,width=20, height=7, borderwidth=5, relief=RIDGE, font=('Arial', 15))
+text_entry1.place(x=20, y=100)
+
+text_entry2 = Text(frame1, width=20, height=7, borderwidth=5, relief=RIDGE, font=('Arial', 15))
+text_entry2.place(x=325, y=100)
+
+root.mainloop()
