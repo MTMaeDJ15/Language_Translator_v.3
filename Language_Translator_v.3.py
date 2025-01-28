a06394e882de7353778c3f20339252a1014e7ec4
unknown <mariateresamaedj19@gmail.com>
2025-01-28 22:43:05 +0800

    Language listbox is now visible, search language also works, and translation function works

--- Language_Translator_v.3.py
+++ Language_Translator_v.3.py
@@ -23,11 +23,11 @@ selected_language = tk.StringVar()
 selected_language.set('English')
 
 language_list = [
-        'Arabic','Cebuano','Chinese','Dutch','English','Filipino','French','German','Greek','Hawaiian','Ilocano',
+        'Arabic','Cebuano','Chinese','Dutch','English','French','German','Greek','Hawaiian','Ilocano',
         'Italian','Japanese','Korean','Latin','Polish','Portuguese','Russian','Spanish','Swedish','Turkish',
 ]
 
-language_codes = { 'Arabic': 'ar', 'Cebuano': 'ceb', 'Chinese': 'zh-cn', 'Dutch': 'nl', 'English': 'en', 'Filipino': 'tl', 'French': 'fr',
+language_codes = { 'Arabic': 'ar', 'Cebuano': 'ceb', 'Chinese': 'zh-cn', 'Dutch': 'nl', 'English': 'en', 'French': 'fr',
     'German': 'de', 'Greek': 'el', 'Hawaiian': 'haw', 'Ilocano': 'ilo','Italian': 'it', 'Japanese': 'ja', 'Korean': 'ko',
     'Latin': 'la', 'Polish': 'pl', 'Portuguese': 'pt', 'Russian': 'ru', 'Spanish': 'es', 'Swedish': 'sv', 'Turkish': 'tr',
 }
@@ -37,9 +37,12 @@ def search_lang(event):
     filtered_lang = [lang for lang in language_list if query in lang.lower()]
     lang_listbox.delete(0, END)
     for lang in filtered_lang:
+        lang_listbox.insert(END, lang)
+    if filtered_lang:
         lang_listbox.place(x=315, y=60)
     else:
         lang_listbox.place_forget()
+        selected_language.set("")
 
 def select_lang(event):
     try: 
@@ -49,25 +52,35 @@ def select_lang(event):
         search_entry.delete(0, END)
         search_entry.insert(0, selected)
     except: 
-        pass
+        search_entry.delete(0, END)
+        selected_language.set("")
 
 def translate():
     lang_1 = text_entry1.get("1.0", "end-1c")
-    cl = selected_language.get()
-    if lang_1 == '':
+    cl = selected_language.get().strip()
+
+    if lang_1.strip() == '':
         messagebox.showerror('Language Translator', 'Enter the Text to Translate!')
-    else:
+        return
+    
+    lang_codes = language_codes.get(cl, None)
+    if not lang_codes:
+        messagebox.showerror('Language Translator', f'Invalid Language Selected! {cl}')
+        return
+
+    try:
         text_entry2.delete(1.0, 'end')
-        translator = Translator()
-        output = translator.translate(lang_1, dest=cl)
+        output = translator.translate(lang_1, dest=lang_codes)
         text_entry2.insert('end', output.text)
+    except Exception as e: 
+        messagebox.showerror('Translation Error', f'Error Occured: {e}')
 
 def clear():
     text_entry1.delete(1.0, 'end')
     text_entry2.delete(1.0, 'end')
 
 search_entry = Entry(frame1, font=('Courier 10 bold'))
-search_entry.place(x=315, y=50, width=200)
+search_entry.place(x=310, y=50, width=250)
 search_entry.insert(0, "Search Language...")
 
 def empty_placeholder(event):
